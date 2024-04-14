d7608efc64ff35349df6eed476de65476307ecc6
Saul Salazar Ozuna <saulsalazar0909@gmai.com>
2024-04-13 17:46:28 -0700

    intro section added, navbar almost done and some changes fot h1 tags to the color attribute

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const switchButton = document.getElementById('switch');
-switchButton.addEventListener('click',()=>{
-    document.body.classList.toggle('o');
-    document.body.classList.toggle('dark');
-    switchButton.classList.toggle('active')
-})
